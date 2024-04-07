56da4cc55c98c20470e6212bff56671176a1986b
amohii <shamchimohammad@gmail.com>
2024-04-07 14:37:12 +0300

    added some Typescript code

--- app.ts
+++ app.ts
@@ -1,1 +1,10 @@
-console.log('Hello World!');
+function add (number1:number,number2:number) {
+    return number1 + number2;
+}
+
+const number1 = 5;
+const number2 = 6;
+
+const result = add(number1, number2);
+
+console.log(result);
